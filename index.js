5e3593202da367dea416f10421ac6e43fa4f8d7b
Mirek Wojtowicz <w.mirek@gmail.com>
2023-05-05 19:36:38 -0400

    test request

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+
+
+// add a route to test if the server is running
+app.get('/test', (req, res) => {
+    console.log("/test request!")
+    res.send('The server is up and running!')
+})
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
 app.listen(process.env.PORT || 3000)
