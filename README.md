509bce95d7f50ef38290bea14f0fa111ec565361
Mirek Wojtowicz <w.mirek@gmail.com>
2023-05-05 20:14:16 -0400

    ping, catch-all, nodemon

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ This is the simplest possible nodejs api using express that responds to any requ
 Yo!
 ```
 
+# Start it locally
+npm start
+nodemon start
+
+
+## Custom modules
+- npm install -g nodemon
+
+
 ### Deploy it in 7 seconds: 
 
 [![Deploy to Cyclic](https://deploy.cyclic.app/button.svg)](https://deploy.cyclic.app/)
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const app = express();
 
 // add a route to test if the server is running
 app.get('/test', (req, res) => {
-    console.log("/test request!")
-    res.send('The server is up and running!')
-})
+  console.log('/test request');
+  res.send('The server is up and running!');
+});
 
+// ping
+app.get('/ping', (req, res) => {
+  console.log('/ping request');
+  res.send('PONG');
+});
+
+// root
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+  console.log('root request');
+  res.send('Yo!');
+});
 
-app.listen(process.env.PORT || 3000)
+// catch all other requests
+app.all('*', (req, res) => {
+  console.log('catch all request: [' + req.url + ']');
+  res.send('Sorry, [' + req.url + '] is an invalid URL.');
+});
+
+// start the server
+port = process.env.PORT || 3000;
+console.log('Starting server on port ' + port);
+app.listen(port);
